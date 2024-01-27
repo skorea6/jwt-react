787ddbf174f7954d5c518084a5e4dcea72f75945
skorea6 <skorea6@naver.com>
2024-01-27 17:41:17 +0900

    Pages Header, Home 추가

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,19 +1,63 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { userId } = useSelector((state) => state.memberInfo);
+
   return (
-    <div className="header">
-      <h1>
-        <Link to="/">JWT 테스트 사이트</Link>
+    <div className="header flex flex-col items-center justify-center text-center py-4">
+      <h1 className="text-2xl font-bold mb-4">
+        <Link to="/" className="text-purple-600 hover:text-purple-800">
+          JWT 인증 사이트 - skorea6
+        </Link>
       </h1>
-      <div className="menu">
-        <Link to="/test1" className="link">
-          테스트1
-        </Link>
-        <Link to="/logout" className="link">
-          로그아웃
-        </Link>
+      <div className="menu flex space-x-4">
+        {userId && (
+          <>
+            <Link
+              to="/member/update"
+              className="link text-blue-500 hover:text-blue-700"
+            >
+              회원정보수정
+            </Link>
+            <Link
+              to="/member/login-list"
+              className="link text-blue-500 hover:text-blue-700"
+            >
+              현재로그인목록
+            </Link>
+          </>
+        )}
+        {/* <Link to="/test2" className="link text-blue-500 hover:text-blue-700">
+          테스트2 (공개)
+        </Link> */}
+        {!userId && (
+          <>
+            <Link
+              to="/auth/login"
+              className="link text-blue-500 hover:text-blue-700"
+            >
+              로그인
+            </Link>
+            <Link
+              to="/auth/signup"
+              className="link text-blue-500 hover:text-blue-700"
+            >
+              회원가입
+            </Link>
+          </>
+        )}
+        {userId && (
+          <Link
+            to="/auth/logout"
+            className="link text-blue-500 hover:text-blue-700"
+          >
+            로그아웃
+          </Link>
+        )}
       </div>
+
+      <hr className="my-4 h-0.5 bg-gray-300 w-full" />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,55 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Home() {
-  const { userId, email, nick } = useSelector((state) => state.token);
+  const { userId, email, nick, name, gender } = useSelector(
+    (state) => state.memberInfo
+  );
 
   return (
-    <div>
-      Home
-      <br />
-      {userId}
-      <br />
-      {email}
-      <br />
-      {nick}
-      <br />
+    <div className="container mx-auto p-4 max-w-md w-full">
+      <h1 className="text-3xl font-bold mb-4">홈 화면</h1>
+
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        {userId ? (
+          <>
+            <p className="text-xl font-bold mb-4">환영합니다!</p>
+            <p>UserID: {userId || ""}</p>
+            <p>Email: {email || ""}</p>
+            <p>Nickname: {nick || ""}</p>
+            <p>Name: {name || ""}</p>
+            <p>Gender: {gender || ""}</p>
+          </>
+        ) : (
+          <p>로그인 유저가 아닙니다.</p>
+        )}
+      </div>
+
+      <div className="flex justify-end mt-4">
+        {userId ? (
+          <Link
+            to="/auth/logout"
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2"
+          >
+            로그아웃
+          </Link>
+        ) : (
+          <>
+            <Link
+              to="/auth/login"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+            >
+              로그인
+            </Link>
+            <Link
+              to="/auth/signup"
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            >
+              회원가입
+            </Link>
+          </>
+        )}
+      </div>
     </div>
   );
 }
